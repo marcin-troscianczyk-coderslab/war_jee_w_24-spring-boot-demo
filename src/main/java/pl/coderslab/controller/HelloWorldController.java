d9275c9a6eb54dcce4cc584f13491842696bb4ab
Troscianczyk, Marcin <marcin.troscianczyk@coderslab.pl>
2023-03-26 15:31:45 +0200

    simple web controller + jsp

--- src/main/java/pl/coderslab/controller/HelloWorldController.java
+++ src/main/java/pl/coderslab/controller/HelloWorldController.java
@@ -1,14 +1,14 @@
 package pl.coderslab.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 class HelloWorldController {
 
     @GetMapping(path = "/hello")
     String helloWorld() {
-        return "Hello world";
+        return "hello";
     }
 
 }
